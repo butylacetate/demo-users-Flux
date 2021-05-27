be9a9b585e2e792973e32f0ca15633722df47f1d
butylacetate <butyl.acetate@gmail.com>
2021-05-27 09:29:06 +0400

    lb

---
+++ src/main/java/org/example/demousers/config/LiquibaseConfig.java
@@ -0,0 +1,45 @@
+package org.example.demousers.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.liquibase.change-log}")
+    private String changeLog;
+
+    @Bean
+    public DataSource getDataSource() {
+
+        return DataSourceBuilder
+                .create()
+                .driverClassName(driverClassName)
+                .username(username)
+                .password(password)
+                .url(datasourceUrl)
+                .build();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(changeLog);
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+}
