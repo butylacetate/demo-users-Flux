6c97cbe167d28ba4877688ea7a8529c32ce9093c
butylacetate <butyl.acetate@gmail.com>
2021-05-27 09:21:51 +0400

    lb-fix

--- src/main/java/org/example/demousers/DemoUsersApplication.java
+++ src/main/java/org/example/demousers/DemoUsersApplication.java
@@ -1,17 +1,7 @@
 package org.example.demousers;
 
-import liquibase.Contexts;
-import liquibase.LabelExpression;
-import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.LiquibaseException;
-import liquibase.resource.ClassLoaderResourceAccessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.Connection;
 
 @SpringBootApplication
 public class DemoUsersApplication {
@@ -19,18 +9,4 @@ public class DemoUsersApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoUsersApplication.class, args);
     }
-
-    public Connection openConnection () throws LiquibaseException {
-
-        java.sql.Connection connection = openConnection(); //your openConnection logic here
-
-        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
-
-        Liquibase liquibase = new liquibase.Liquibase("classpath:/db/changelog/db.changelog-master.xml", new ClassLoaderResourceAccessor(), database);
-
-        liquibase.update(new Contexts(), new LabelExpression());
-        return connection;
-    }
-
-
 }
